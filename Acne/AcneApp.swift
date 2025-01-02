d9688beaeafc25a7e6e98d7dfd0f24a5d1f218ef
TimRiedel <riedel_tim@icloud.com>
2025-01-02 10:47:32 +0100

    Initial Commit

---
+++ Acne/AcneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcneApp.swift
+//  Acne
+//
+//  Created by Tim Riedel on 02.01.25.
+//
+
+import SwiftUI
+
+@main
+struct AcneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AcneTests/AcneTests.swift
@@ -0,0 +1,16 @@
+//
+//  AcneTests.swift
+//  AcneTests
+//
+//  Created by Tim Riedel on 02.01.25.
+//
+
+import Testing
+
+struct AcneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
